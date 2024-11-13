c850e249ce02388453964c4cd4b5ae12a0070c8c
Emily Nieves <nemily1710@gmail.com>
2024-11-13 14:06:31 -0500

    feat: :tada: Se inicializa repositorio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+                display: ['Baloo 2', 'cursive'],
+            },
+            colors: {
+                primary: '#325F7B',
+                background: '#FDF9F3',
+                backgroundT: '#F6F0E4',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
